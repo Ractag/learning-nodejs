4e2b24bb219bd758f5a78e7fc4ce5f583a384782
Victor G <victorgj95@outlook.com>
2023-10-03 10:47:23 +0200

    Playing with node.js and chalk

---
+++ script.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const wilders = ["Mathieu", "Nélia", "Marie", "Fred"];
+
+console.log(chalk.red(`Hello ${wilders[0]}`));
+console.log(chalk.green(`Hello ${wilders[1]}`));
+console.log(chalk.blue(`Hello ${wilders[2]}`));
+console.log(chalk.yellow(`Hello ${wilders[3]}`));
